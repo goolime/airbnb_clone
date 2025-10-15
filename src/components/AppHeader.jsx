bee9b2ec984c7271e3c1254c258c9e926cab4cfc
DekelAH <54857125+DekelAH@users.noreply.github.com>
2025-10-16 00:24:50 +0300

    Header responsive fixes

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -10,19 +10,46 @@ export function AppHeader() {
 
     return <>
 
-        <div className='fixed grid col lg:grid-cols-3 lg:grid-rows-none md:grid-cols-2 md:grid-rows-2 w-full max-h-30 bg-white items-center z-10 border-x-[1px] border-gray-100 shadow-sm px-5'>
-            <div className='hidden md:block py-4'>
+        <div className='
+        fixed 
+        grid 
+        lg:grid-cols-[auto_minmax(700px,850px)_auto] 
+        lg:grid-rows-none 
+        sm:grid-cols-2 
+        sm:grid-rows-2 
+        w-full 
+        bg-white 
+        items-center 
+        z-10 
+        border-x-[1px] 
+        border-gray-100 
+        shadow-sm px-5
+        '>
+            <div className='hidden sm:block py-4'>
                 <img
                     src={logoImage}
-                    className='hidden md:block cursor-pointer'
+                    className='hidden sm:block cursor-pointer'
                     alt="logo-image"
                     height="50"
                     width="50"
                 />
             </div>
             {/* Search Bar */}
-            <div className='w-full max-h-5px border-[1px] lg:col-[2/3] border-gray-100 py-2 sm:col-span-2 rounded-full shadow-md hover:shadow-lg transition cursor-pointer'>
-                <div className="grid grid-cols-3 items-center justify-between">
+            <div className='
+            w-full 
+            max-h-5px 
+            border-[1px] 
+            my-3 
+            lg:col-[2/3] 
+            border-gray-200 
+            py-2 
+            sm:col-span-2 
+            rounded-full 
+            shadow-md 
+            hover:shadow-md 
+            transition 
+            cursor-pointer'>
+                <div className="hidden sm:grid grid-cols-3 items-center justify-between">
                     <div className='flex flex-col text-sm font-semibold px-6'>
                         Where
                         <input type="text" className='focus:outline-none placeholder:font-thin placeholder:text-gray-400' placeholder='Search destinations' />
@@ -41,15 +68,19 @@ export function AppHeader() {
                         </div>
                     </div>
                 </div>
+                <div className='flex flex-row items-center justify-center sm:hidden'>
+                    <BiSearch size={18}/>
+                    <span className='font-semibold ml-2'>Start your search</span>
+                </div>
             </div>
-            <div className='flex flex-row items-center justify-end gap-3 md:col-[2/3] md:row-[1/2] lg:col-[3/4]'>
+            <div className='flex flex-row items-center justify-end gap-3 sm:col-[2/3] sm:row-[1/2] lg:col-[3/4]'>
                 <div onClick={() => { }} className='hidden lg:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'>
                     Become a host
                 </div>
-                <div className='hidden md:block'>
-                    <img src={userImage} className='rounded-full' height={30} width={30} alt='user-image' />
+                <div className='hidden sm:block'>
+                    <img src={userImage} className='rounded-full' height={40} width={40} alt='user-image' />
                 </div>
-                <div onClick={() => { }} className='hidden md:block p-4 md:py-1 md:py-2 bg-gray-300 flex flex row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
+                <div onClick={() => { }} className='hidden sm:block p-4 sm:py-1 sm:py-2 bg-gray-300 flex flex row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
                     <AiOutlineMenu />
                 </div>
             </div>
