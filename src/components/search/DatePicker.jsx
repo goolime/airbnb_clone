761b95adbd7c43bc49e2a86bdf95efe975ed0ac5
Gal Meir <goolime@gmail.com>
2025-10-19 17:33:59 +0300

    feat: enhance DatePicker component with custom styling and range selection functionality

--- src/components/search/DatePicker.jsx
+++ src/components/search/DatePicker.jsx
@@ -1,23 +1,52 @@
 import { useState } from "react"
-import { DayPicker } from "react-day-picker"
+import { DayPicker, getDefaultClassNames, NextMonthButton } from "react-day-picker"
 
 
 export function DatePicker() {
 
-    const [selected, setSelected] = useState(new Date())
-
+    const [date, setDate] = useState(new Date(Date.now()));
+    const defaultClassNames = getDefaultClassNames();
+    console.log('defaultClassNames', defaultClassNames);
+    const now= new Date(Date.now());
     return <>
-        <DayPicker
-        className="cursor-pointer"
-            animate
+        <div className="flex justify-center">
+            <DayPicker
             mode="range"
-            defaultMonth={new Date()}
-            selected={selected}
-            onSelect={setSelected}
-            footer={
-                selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+            navLayout="around"
+            fixedWeeks={true}
+            disabled={{ before: now, after: new Date(now.getFullYear() + 1, now.getMonth(), now.getDate()) }}
+            startMonth={new Date(now.getFullYear(), now.getMonth())}
+            endMonth={new Date(now.getFullYear()+1,now.getMonth())}
+            classNames={{
+              months: "flex sm:flex-row gap-4",
+              day_button: "",
+              button_previous: "m-2 p-2 rounded-full hover:bg-gray-200 absolute left-0 top-1/2 -translate-y-1/2 aria-disabled:hidden ",
+              button_next: "m-2 p-2 rounded-full hover:bg-gray-200 absolute right-0 top-1/2 -translate-y-1/2 aria-disabled:hidden ",
+              day: "sm:size-[6dvw] sm:min-w-[45.06px] sm:min-h-[45.05px] md:size-[57.88px] lg:size-[3.8dvw]  lg:min-w-[42.95px] lg:min-h-[42.95px] xl:size-[54.55px] text-center",
+              range_start: "rounded-l-full text-white rangeStart",
+              range_end: "rounded-r-full text-white rangeEnd has-[button]:shadow-[-1em_0px_0.6em_rgba(0,0,0,0.1)]",
+              selected: "has-[button]:bg-gray-200",
+              disabled: "opacity-20",
+            }}
+            components={
+              {
+                Day:(props) => {
+                  const{day,modifiers,...tdProps}=props;
+                  const Month=day.date.getMonth()
+                  if(day.date.getDate()===1){
+                    tdProps.className+=" start-of-month"
+                  }
+                  if (Month!==(new Date(day.date.getTime()+1000*60*60*24)).getMonth()){
+                    tdProps.className+=" end-of-month"
+                  }
+                  return <td {...tdProps}/>
+                }
+              }
             }
-        />
-
+            />
+        </div>
     </>
 }
