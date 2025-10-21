298c25e92a9bbb000993ea53f8be8412b5967ade
DekelAH <54857125+DekelAH@users.noreply.github.com>
2025-10-21 09:27:03 +0300

    Map changes

--- src/components/AppMap.jsx
+++ src/components/AppMap.jsx
@@ -3,15 +3,18 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 
 import { Map, Popup, Marker } from 'react-map-gl/mapbox'
 import { getCenter } from 'geolib'
-import { useState } from 'react';
-const demoCity = { countryCode: 'FR', city: 'Paris', minLat: 48.8156, maxLat: 48.9022, minLng: 2.2241, maxLng: 2.4699 }
-export function AppMap({ searchResults }) {
+import { useState } from 'react'
+
+export function AppMap({ searchResults, location }) {
 
     const [selectedLocation, setSelectedLocation] = useState({})
 
+    console.log('searchResults in AppMap:', searchResults);
+    console.log('location in AppMap:', location);
+
     const coordinates = [
-        { latitude: demoCity.maxLat, longitude: demoCity.minLng },
-        { latitude: demoCity.minLat, longitude: demoCity.maxLng }
+        { latitude: location.maxLat, longitude: location.minLng },
+        { latitude: location.minLat, longitude: location.maxLng }
     ]
 
     const center = getCenter(coordinates)
@@ -35,9 +38,8 @@ export function AppMap({ searchResults }) {
                     borderRadius: '15px'
                 }}
                 mapStyle="mapbox://styles/tapuchips/cmgyvwabb007c01quf1ng3u4d"
-
             >
-                {
+                {/* {
                     <Marker
                         longitude={center.longitude}
                         latitude={center.latitude}
@@ -45,27 +47,45 @@ export function AppMap({ searchResults }) {
                         offsetTop={-10}
                     >
 
-                        <div onClick={() => { setSelectedLocation()}} className='cursor-pointer shadow-sm animate-bounce flex flex-row justify-center items-center bg-(--headerBg) w-[66px] h-[28] border-1 border-gray-300 rounded-lg'>
+                        <div onClick={() => { setSelectedLocation() }} className='cursor-pointer shadow-sm animate-bounce flex flex-row justify-center items-center bg-(--headerBg) w-[66px] h-[28] border-1 border-gray-300 rounded-lg'>
                             <label className='text-md cursor-pointer'>3223$</label>
                             <p className=" text-sm cursor-pointer" aria-label="push-pin">📌</p>
                         </div>
 
                     </Marker>
-                }
-                {/* {searchResults &&
+                } */}
+                {searchResults &&
                     searchResults.map(result => (
                         <div key={result.long}>
                             <Marker
-                                longitude={result.long}
+                                longitude={result.lng}
                                 latitude={result.lat}
                                 offsetLeft={-20}
                                 offsetTop={-10}
                             >
-                                <p className="cursor-pointer text-2xl animate-bounce" aria-label="push-pin">📌</p>
+                                <div
+                                    onClick={() => { setSelectedLocation(result) }}
+                                    className='
+                                    cursor-pointer 
+                                    shadow-sm 
+                                    animate-bounce 
+                                    flex flex-row 
+                                    justify-center 
+                                    items-center 
+                                    bg-(--headerBg) 
+                                    w-[66px] 
+                                    h-[28] 
+                                    border-1 
+                                    border-gray-300 
+                                    rounded-lg'
+                                >
+                                    <label className='text-md cursor-pointer'>{result.price}</label>
+                                    <p className=" text-sm cursor-pointer" aria-label="push-pin">📌</p>
+                                </div>
                             </Marker>
                         </div>
                     ))
-                } */}
+                }
             </Map>
         </>
     )
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,33 +2,33 @@ import { ListPreview } from "../components/preview/list"
 import { property } from "../../templates/property.js"
 import { AppMap } from "../components/AppMap.jsx"
 import { useSearchParams } from "react-router"
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import { propertiesService } from "../services/properties.service.js"
 import { getProperties } from "../actions/explore.actions.js"
 
-export function SearchPage(){
-    const [searchParams,setSearchParams] = useSearchParams()
+export function SearchPage() {
+    const [searchParams, setSearchParams] = useSearchParams()
     const [filterData, setFilterData] = useState(null)
     const [properties, setProperties] = useState([])
     const [page, setPage] = useState(1)
     const [maxPage, setMaxPage] = useState(1)
     const [loading, setLoading] = useState(true)
 
-    useEffect(()=>{
+    useEffect(() => {
         const filter = propertiesService.getFilterFromSearchParams(searchParams)
         setFilterData(filter)
-    },[searchParams])
+    }, [searchParams])
 
-    useEffect(()=>{
-        getProperties(filterData, 1).then(({newProperties, newMaxPage})=>{
+    useEffect(() => {
+        getProperties(filterData, 1).then(({ newProperties, newMaxPage }) => {
             setPage(1)
             setProperties(newProperties)
             setMaxPage(newMaxPage)
             setLoading(false)
         })
-    },[filterData])
+    }, [filterData])
 
-
+    if (loading) return <div>Loading...</div>
     return (
         <div className="grid grid-cols-2">
             <div className="px-12 py-5">
@@ -36,7 +36,7 @@ export function SearchPage(){
                 <ListPreview properties={properties} />
             </div>
             <div className="sticky top-0 h-screen py-5">
-                <AppMap searchResults={searchResults} />
+                <AppMap searchResults={properties} location={filterData?.loc} />
             </div>
         </div>
     )
