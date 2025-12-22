3dcc37ad9ddadc13701eccfed60bcd72914b8502
DekelAH <54857125+DekelAH@users.noreply.github.com>
2025-12-22 21:00:42 +0200

    refactor: Update project name references from 'airbnb' to 'airdnd' and clean up unused code

--- src/components/search/Location.jsx
+++ src/components/search/Location.jsx
@@ -1,4 +1,6 @@
 import { getCities } from "../../actions/explore.actions.js"
+import { BsBank } from "react-icons/bs"
+import { TbLocation } from "react-icons/tb"
 
 const citiesAdvantages = [
 
@@ -27,7 +29,9 @@ export function Location({ onFilterChange }) {
             <div className="flex flex-col">
                 <label className="px-6 mb-2 w-full text-sm text-[#222222]">Suggested destinations</label>
                 <div className="flex flex-row p-2 mx-4 hover:bg-gray-200 rounded-lg cursor-pointer">
-                    <img height={55} width={55} className="mr-3" src="https://a0.muscache.com/im/pictures/airbnb-platform-assets/AirbnbPlatformAssets-hawaii-autosuggest-destination-icons-2/original/ea5e5ee3-e9d8-48a1-b7e9-1003bf6fe850.png" alt="icon" />
+                    <div className="flex w-14 h-14 justify-center items-center bg-blue-50 rounded-lg mr-3">
+                        <TbLocation className="text-blue-400 w-6 h-6" />
+                    </div>
                     <div className="flex flex-col justify-center text-sm">
                         <label className="font-semibold cursor-pointer">Nearby</label>
                         <span className="text-gray-500">Find what's around you</span>
@@ -38,7 +42,9 @@ export function Location({ onFilterChange }) {
                 {cities &&
                     cities.map((city, idx) => (
                         <li key={idx} className="flex flex-row p-2 mx-4 hover:bg-gray-200 rounded-lg cursor-pointer mb-1" onClick={() => handleCityClick(city)}>
-                            <img height={55} width={55} className="mr-3" src="https://a0.muscache.com/im/pictures/airbnb-platform-assets/AirbnbPlatformAssets-hawaii-autosuggest-destination-icons-1/original/c9333e22-1d8d-4d6c-80db-615f4c2d4cc4.png" alt="icon" />
+                            <div className="flex w-14 h-14 justify-center items-center bg-rose-50 rounded-lg mr-3">
+                                <BsBank className="text-rose-400 w-6 h-6" />
+                            </div>
                             <div className="flex flex-col justify-center text-sm">
                                 <label className="font-semibold cursor-pointer">{city.city}, {city.countryCode}</label>
                                 <span className="text-gray-500">{getRandomAdvantage()}</span>
